d152d62bf8c75b21dded22cb327feae48dbcd4a5
yagen <destiny_yagen@qq.com>
2025-12-23 08:58:15 +0800

    add 1.21.5

---
+++ neoforge-1.21.5/src/main/java/yagen/waitmydawn/LootStorageScreen.java
@@ -0,0 +1,55 @@
+package yagen.waitmydawn;
+
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.player.Inventory;
+import net.neoforged.neoforge.network.PacketDistributor;
+
+public class LootStorageScreen extends AbstractContainerScreen<LootStorageMenu> {
+    private static final ResourceLocation TEXTURE = ResourceLocation.parse("textures/gui/container/shulker_box.png");
+
+    public LootStorageScreen(LootStorageMenu menu, Inventory playerInventory, Component title) {
+        super(menu, playerInventory, title);
+        this.imageWidth = 176;
+        this.imageHeight = 166;
+        this.inventoryLabelY = this.imageHeight - 93;
+    }
+
+    @Override
+    protected void init() {
+        super.init();
+        this.addRenderableWidget(Button.builder(Component.literal("<"), button -> {
+            PacketDistributor.sendToServer(new PacketChangePage(-1));
+        }).bounds(this.leftPos - 25, this.topPos, 20, 20).build());
+
+        this.addRenderableWidget(Button.builder(Component.literal(">"), button -> {
+            PacketDistributor.sendToServer(new PacketChangePage(1));
+        }).bounds(this.leftPos + this.imageWidth + 5, this.topPos, 20, 20).build());
+    }
+
+    @Override
+    public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
+        super.render(guiGraphics, mouseX, mouseY, partialTick);
+        this.renderTooltip(guiGraphics, mouseX, mouseY);
+
+        guiGraphics.drawString(this.font,
+                (this.menu.getCurrentPage() + 1) + " / " + Math.max(1, this.menu.getTotalPages()),
+                this.leftPos + 80, this.topPos - 10, 0xFFFFFF, true);
+    }
+
+    @Override
+    protected void renderBg(GuiGraphics guiGraphics, float partialTick, int mouseX, int mouseY) {
+        guiGraphics.blit(
+                RenderType::guiTextured,
+                TEXTURE,
+                leftPos, topPos,
+                0, 0,
+                imageWidth, imageHeight,
+                256, 256
+        );
+    }
+}
---
+++ neoforge-1.21.5/src/main/java/yagen/waitmydawn/NetworkHandler.java
@@ -0,0 +1,29 @@
+package yagen.waitmydawn;
+
+import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
+import net.neoforged.neoforge.network.registration.PayloadRegistrar;
+
+public class NetworkHandler {
+
+    public static void register(RegisterPayloadHandlersEvent event) {
+        PayloadRegistrar registrar = event.registrar(PackUp.MODID);
+
+        registrar.playToServer(
+                PacketStoreLoot.TYPE,
+                PacketStoreLoot.STREAM_CODEC,
+                PacketStoreLoot::handle
+        );
+
+        registrar.playToServer(
+                PacketOpenStorage.TYPE,
+                PacketOpenStorage.STREAM_CODEC,
+                PacketOpenStorage::handle
+        );
+
+        registrar.playToServer(
+                PacketChangePage.TYPE,
+                PacketChangePage.STREAM_CODEC,
+                PacketChangePage::handle
+        );
+    }
+}
